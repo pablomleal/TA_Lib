72639ceeabd15cc57aba4dfb5ef8f3971a04b4b7
Pablo Miguel <pablomleal@gmail.com>
2023-05-20 09:13:01 +0200

    Created fromCSV function to used cached data

--- functions/retrieve.py
+++ functions/retrieve.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import yfinance as yf
 
+
+
+
 def get_tickers(numberOfStocks=10):
     tickers_df = pd.read_csv('data/sp500_tickers.csv').head(numberOfStocks)
     tickers_df.set_index('Ticker', inplace=True)
@@ -22,3 +25,36 @@ def get_downloaded_data(tickers_df, numberOfStocks = 10, daysSince=14):
     downloadedData = yf.download(tickers_string, start=ts_origin, end=ts_today).fillna(0)
     return (downloadedData)
 
+def distribute_data(downloadedData):
+    rawdata = downloadedData['Close']
+    spreads = (downloadedData['High'] - downloadedData['Low'])/(downloadedData['Close'])
+    volumes = downloadedData['Volume']
+
+    return (rawdata, spreads, volumes)
+
+def save_data(rawdata, spreads, volumes):
+    rawdata.to_csv('./data/close.csv')
+    spreads.to_csv('./data/spreads.csv')
+    volumes.to_csv('./data/volumes.csv')
+
+def from_CSV(daysSince, numberOfStocks):
+    
+    #Retrieving stored data
+    closes_saved = pd.read_csv('./data/close.csv').set_index('Date')
+    
+    #Checking same indices
+    ts_origin = pd.to_datetime('today') - pd.Timedelta(days=daysSince)
+    is_same_dates = ts_origin.strftime('%Y-%m-%d') == closes_saved.index[0]
+
+    #Checking same number of columns (stocks)
+    is_same_number_of_stocks = closes_saved.shape[1] == numberOfStocks
+    
+    print (f"Is it possible to use cached data? {is_same_dates & is_same_number_of_stocks}")
+    return (is_same_dates & is_same_number_of_stocks)
+
+def get_from_CSV():
+        print ("Returning stored data.")
+        rawdata = (pd.read_csv('data/close.csv')).set_index('Date')
+        spreads = (pd.read_csv('data/spreads.csv')).set_index('Date')
+        volumes = (pd.read_csv('data/volumes.csv')).set_index('Date')
+        return (rawdata, spreads, volumes)
